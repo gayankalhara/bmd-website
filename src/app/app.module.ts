94e0de912fbd317b54591eb03490ff43fca6ae22
Gayan Kalhara <gayan.csnc@gmail.com>
2017-01-21 01:05:08 +0530

    UI Improvements + Setup Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NewsComponent } from './pages/news/news.component';
+import { MyAccountComponent } from './pages/my-account/my-account.component';
 
 
 @NgModule({
@@ -17,6 +20,9 @@ import { ContactComponent } from './pages/contact/contact.component';
     LayoutComponent,
     HomeComponent,
     ContactComponent,
+    AboutComponent,
+    NewsComponent,
+    MyAccountComponent,
 
   ],
   imports: [
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,18 +1,32 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import {AboutComponent} from "./about/about.component";
+import {NewsComponent} from "./news/news.component";
+import {MyAccountComponent} from "./my-account/my-account.component";
 
 
 
 export const PAGE_ROUTES: Routes = [
-
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-      path: 'contact',
-      component: ContactComponent
-    }
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'my-account',
+    component: MyAccountComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
 
 ];
